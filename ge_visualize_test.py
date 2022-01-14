4836b1b19b977108cb91a46db1a5d17c8bb9fbc9
toi <toi@ucl.nuee.nagoya-u.ac.jp>
2022-01-14 12:06:44 +0900

    初期版（動作確認済み）

--- ge_visualize_test.py
+++ ge_visualize_test.py
@@ -1,56 +1,88 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-# import time
-# import busio
-# import board
-
-# import chardet
-# import os
-# import sys 
-# import logging
-# import spidev as SPI
-# sys.path.append("..")
-# from lib import LCD_1inch28
-# from PIL import Image,ImageDraw,ImageFont
-# import matplotlib.pyplot as plt
-
-import smbus
+#import chardet
+import os
+import sys 
+import time
+import logging
 import numpy as np
+import spidev as SPI
+sys.path.append("..")
+from lib import LCD_1inch28
+from PIL import Image,ImageDraw,ImageFont
+from GridEye import GridEye
 
-class GridEye:
-    __REG_FPSC = 0x02
-    __REG_TOOL = 0x0E
-    __REG_PIXL = 0x80
 
-    def __init__(self, address=0x19):
-        self.i2c = smbus.SMBus(1)   # 0 for Raspberry Pi Model B(256MB)
-        self.address = address
-        self.i2c.write_byte_data(self.address, self.__REG_FPSC, 0x00)
+myeye = GridEye(0x19)
+pixel = np.array(myeye.pixelOut())
+pixel.resize((8, 8))
 
-    def thermistorTemp(self):
-        result = self.i2c.read_word_data(self.address, self.__REG_TOOL)
-        if(result > 2047):
-            result = result - 2048
-            result = -result
-        return result * 0.0625
+print("--------------------------------")
+print("pixel={}".format(pixel))
+print("--------------------------------")
 
-    def pixelOut(self):
-        out = []
-        for x in xrange(0, 64):
-            temp = self.i2c.read_word_data(
-                self.address, self.__REG_PIXL + (x * 2))
-            if(temp > 2047):
-                temp = temp - 4096
-            out.append(temp * 0.25)
-        return out
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0
+device = 0
+logging.basicConfig(level=logging.DEBUG)
+try:
+    # display with hardware SPI:
+    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+    #disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp = LCD_1inch28.LCD_1inch28()
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
 
-myeye = GridEye()
-pixel = myeye.pixelOut()
-# pixel = np.array(myeye.pixelOut())
+    # Create blank image for drawing.
+    image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw = ImageDraw.Draw(image1)
 
-# pixel.resize((8, 8))
+    #logging.info("draw point")
+    #draw.rectangle((Xstart,Ystart,Xend,Yend), fill = "color")
+    logging.info("draw circle")
+    draw.arc((1,1,239,239),0, 360, fill =(0,0,255))
+    draw.arc((2,2,238,238),0, 360, fill =(0,0,255))
+    draw.arc((3,3,237,237),0, 360, fill =(0,0,255))
+    
+    logging.info("draw dial line")
+    draw.line([(120, 1),(120, 12)], fill = (128,255,128),width = 4)
+    draw.line([(120, 227),(120, 239)], fill = (128,255,128),width = 4)
+    draw.line([(1,120),(12,120)], fill = (128,255,128),width = 4)
+    draw.line([(227,120),(239,120)], fill = (128,255,128),width = 4)
 
-# print("--------------------------------")
-# print("pixel={}".format(pixel))
-# print("--------------------------------")
+    logging.info("draw text")
+    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
+    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+
+    draw.text((40, 50), 'WaveShare', fill = (128,255,128),font=Font2)
+    text= u"微雪电子"
+    draw.text((74, 150),text, fill = "WHITE",font=Font3)
+    
+    logging.info("draw pointer line")
+    draw.line([(120, 120),(70, 70)], fill = "YELLOW",width = 3)
+    draw.line([(120, 120),(176, 64)], fill = "BLUE",width = 3)
+    draw.line([(120, 120),(120 ,210)], fill = "RED",width = 3)   
+ 
+    im_r=image1.rotate(180)
+    disp.ShowImage(im_r)
+    time.sleep(3)
+    logging.info("show image")
+    image = Image.open('../pic/LCD_1inch28_1.jpg')	
+    im_r=image.rotate(180)
+    disp.ShowImage(im_r)
+    time.sleep(3)
+    disp.module_exit()
+    logging.info("quit:")
+except IOError as e:
+    logging.info(e)    
+except KeyboardInterrupt:
+    disp.module_exit()
+    logging.info("quit:")
+    exit()
---
+++ grideye_visualize_LCD/ge_visualize_test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#import chardet
+import os
+import sys 
+import time
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import spidev as SPI
+sys.path.append("..")
+from lib import LCD_1inch28
+from PIL import Image,ImageDraw,ImageFont
+from GridEye import GridEye
+
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0
+device = 0
+try:
+    # display with hardware SPI:
+    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+    #disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp = LCD_1inch28.LCD_1inch28()
+    # Initialize library.
+    disp.Init()
+
+    # Clear display.
+    disp.clear()
+
+    # Create blank image for drawing.
+    # image1 = Image.new("RGB", (disp.width, disp.height), "WHITE") # width=240 height=240
+    # draw = ImageDraw.Draw(image1)
+
+    # im_r=image1.rotate(180)
+    # disp.ShowImage(im_r)
+    # time.sleep(3)
+
+    print("currentdpi={}, currentfigsize={}".format(mpl.rcParams['figure.dpi'], mpl.rcParams['figure.figsize']))
+    plt.figure(figsize=(2, 2))
+    
+    # image = Image.open('../pic/ge_heatmap.jpg')
+    # # image = Image.open('../pic/LCD_1inch28_1.jpg')
+    # im_r=image.rotate(180)
+    # disp.ShowImage(im_r)
+    # disp.module_exit()
+
+    count = 0
+    while(1):
+        # Get grideye data
+        myeye = GridEye(0x19)
+        pixel = np.array(myeye.pixelOut())
+        pixel.resize(8, 8)
+
+        # Convert GrideyeData to heatmap image
+        sns.heatmap(pixel, annot=True, cbar=False, square=True, xticklabels=False, yticklabels=False)
+        plt.savefig("../pic/ge_heatmap.jpg", dpi=120)
+        plt.clf()
+        
+        image = Image.open('../pic/ge_heatmap.jpg')
+        im_r=image.rotate(180)
+        disp.ShowImage(im_r)
+        print("count: {}".format(count))
+        count += 1
+except IOError as e:
+    print(e)
+except KeyboardInterrupt:
+    disp.module_exit()
+    exit()
